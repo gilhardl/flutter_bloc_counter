0c310249a0bc12e1252ea7ce6c737259433f44a8
Lucas GILHARD <l.gilhard@gmail.com>
2020-03-20 15:35:58 +0100

    add Counter BLoC

---
+++ lib/blocs/counter/counter_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_counter/blocs/counter/counter_event.dart';
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield _incrementCounter();
+        break;
+      case CounterEvent.decrement:
+        yield _decrementCounter();
+        break;
+    }
+  }
+
+  int _incrementCounter() => state + 1;
+
+  int _decrementCounter() => state - 1;
+}
